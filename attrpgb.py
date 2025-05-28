b04a4b0be8baea0b2195f8927e5e4f8d1e4f27c5
Hammy2015 <hammy2015@gmail.com>
2025-05-27 22:33:53 -0400

    Added Profession logic

--- attrpgb.py
+++ attrpgb.py
@@ -1,5 +1,6 @@
 import discord
 import magic_items as mi
+import roller as r
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,6 +25,7 @@ async def on_message(message):
     if message.content.startswith('!attrpgb'):
         await message.channel.send('Hello! This is the attrpgb bot. How can I assist you today?')
 
+    # Magic item generator logic, uses logic determined here: https://www.d20pfsrd.com/magic-items/wondrous-items/
     if message.content.startswith('!mig'):
         price = int(message.content.split()[-1])
         tier = message.content.split()[-2]
@@ -34,7 +36,7 @@ async def on_message(message):
             item = mi.mig(price,tier)
         await message.channel.send(item)
 
-
+    # Dice rolling logic, real basic for now
     if message.content.startswith('!roll'):
         dice_sides = int(message.content.split()[1].split('d')[1])
         num_dice = int(message.content.split()[1].split('d')[0])
@@ -42,6 +44,18 @@ async def on_message(message):
             roll_mod = int(message.content.split('+')[1])
         elif message.content.contains('-'):
             roll_mod = -int(message.content.split('-')[1])
+
+    # TODO: profession rules for making money https://www.d20pfsrd.com/skills/profession/
+    if message.content.startswith('!prof'):
+        days = message.content.split()[1]
+        skill = message.content.split()[2]
+        total = 0
+        for x in range(0, int(days)):
+            total = total + r.roll_dice_with_advantage(1, 20, True) + int(skill)
+
+        await message.channel.send(f'You made {total} gp in {days} days of work.')
+
+    # TODO: 
         
 
 client.run(keystring)
